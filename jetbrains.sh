91235000525863fe164e4713d19c1b2dfa2c06ff
mranv <iamanubhavgain@gmail.com>
2024-07-11 14:32:25 +0530

    chore: Update JetBrains Toolbox installation script
    Update the JetBrains Toolbox installation script to download version 2.4.0.32175 of the toolbox and make it executable. This ensures that the latest version of the toolbox is used and allows for easy execution of the script.

--- jetbrains.sh
+++ jetbrains.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
-# Download JetBrains Toolbox
-curl -L https://download-cdn.jetbrains.com/toolbox/jetbrains-toolbox-2.4.0.32175.tar.gz -o jetbrains-toolbox-2.4.0.32175.tar.gz
+set -e
+set -o pipefail
 
-# Extract the downloaded tar.gz file
-tar -xzvf jetbrains-toolbox-2.4.0.32175.tar.gz
+TMP_DIR="/tmp"
+INSTALL_DIR="$HOME/.local/share/JetBrains/Toolbox/bin"
+SYMLINK_DIR="$HOME/.local/bin"
 
-# Change directory to the extracted folder
-cd jetbrains-toolbox-2.4.0.32175/
+echo -e "\e[94mFetching the URL of the latest version...\e[39m"
+ARCHIVE_URL=$(curl -s 'https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release' | grep -Po '"linux":.*?[^\\]",' | awk -F ':' '{print $3,":"$4}'| sed 's/[", ]//g')
+ARCHIVE_FILENAME=$(basename "$ARCHIVE_URL")
 
-# Make the toolbox executable
-chmod +x jetbrains-toolbox
+echo -e "\e[94mDownloading $ARCHIVE_FILENAME...\e[39m"
+rm "$TMP_DIR/$ARCHIVE_FILENAME" 2>/dev/null || true
+wget -q --show-progress -cO "$TMP_DIR/$ARCHIVE_FILENAME" "$ARCHIVE_URL"
 
-# Run JetBrains Toolbox
-./jetbrains-toolbox
+echo -e "\e[94mExtracting to $INSTALL_DIR...\e[39m"
+mkdir -p "$INSTALL_DIR"
+rm "$INSTALL_DIR/jetbrains-toolbox" 2>/dev/null || true
+tar -xzf "$TMP_DIR/$ARCHIVE_FILENAME" -C "$INSTALL_DIR" --strip-components=1
+rm "$TMP_DIR/$ARCHIVE_FILENAME"
+chmod +x "$INSTALL_DIR/jetbrains-toolbox"
+
+echo -e "\e[94mSymlinking to $SYMLINK_DIR/jetbrains-toolbox...\e[39m"
+mkdir -p $SYMLINK_DIR
+rm "$SYMLINK_DIR/jetbrains-toolbox" 2>/dev/null || true
+ln -s "$INSTALL_DIR/jetbrains-toolbox" "$SYMLINK_DIR/jetbrains-toolbox"
+
+if [ -z "$CI" ]; then
+	echo -e "\e[94mRunning for the first time to set-up...\e[39m"
+	( "$INSTALL_DIR/jetbrains-toolbox" & )
+	echo -e "\n\e[32mDone! JetBrains Toolbox should now be running, in your application list, and you can run it in terminal as jetbrains-toolbox (ensure that $SYMLINK_DIR is on your PATH)\e[39m\n"
+else
+	echo -e "\n\e[32mDone! Running in a CI -- skipped launching the AppImage.\e[39m\n"
+fi
