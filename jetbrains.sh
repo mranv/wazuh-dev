47b7c9569b9a56d4534449113dc11ebd4fa97388
mranv <iamanubhavgain@gmail.com>
2024-07-12 09:53:21 +0530

    chore: The wget command on some distributions might not support the --show-progress option. To ensure compatibility across different environments, you can remove the --show-progress option.

--- jetbrains.sh
+++ jetbrains.sh
@@ -10,30 +10,35 @@ TMP_DIR="/tmp"
 INSTALL_DIR="$HOME/.local/share/JetBrains/Toolbox/bin"
 SYMLINK_DIR="$HOME/.local/bin"
 
-echo -e "\e[94mFetching the URL of the latest version...\e[39m"
-ARCHIVE_URL=$(curl -s "$API_URL" | grep -Po '"linux":.*?[^\\]",' | awk -F ':' '{print $3,":"$4}'| sed 's/[", ]//g')
+# Colors
+BLUE='\033[1;34m'
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+
+echo -e "${BLUE}Fetching the URL of the latest version...${NC}"
+ARCHIVE_URL=$(curl -s "$API_URL" | grep -Po '"linux":.*?[^\\]",' | awk -F ':"' '{print $2}' | sed 's/[", ]//g')
 ARCHIVE_FILENAME=$(basename "$ARCHIVE_URL")
 
-echo -e "\e[94mDownloading $ARCHIVE_FILENAME...\e[39m"
-rm "$TMP_DIR/$ARCHIVE_FILENAME" 2>/dev/null || true
-wget -q --show-progress -cO "$TMP_DIR/$ARCHIVE_FILENAME" "$ARCHIVE_URL"
+echo -e "${BLUE}Downloading $ARCHIVE_FILENAME...${NC}"
+rm -f "$TMP_DIR/$ARCHIVE_FILENAME" 2>/dev/null || true
+wget -q -O "$TMP_DIR/$ARCHIVE_FILENAME" "$ARCHIVE_URL"
 
-echo -e "\e[94mExtracting to $INSTALL_DIR...\e[39m"
+echo -e "${BLUE}Extracting to $INSTALL_DIR...${NC}"
 mkdir -p "$INSTALL_DIR"
-rm "$INSTALL_DIR/jetbrains-toolbox" 2>/dev/null || true
+rm -rf "$INSTALL_DIR/*" 2>/dev/null || true
 tar -xzf "$TMP_DIR/$ARCHIVE_FILENAME" -C "$INSTALL_DIR" --strip-components=1
-rm "$TMP_DIR/$ARCHIVE_FILENAME"
+rm -f "$TMP_DIR/$ARCHIVE_FILENAME"
 chmod +x "$INSTALL_DIR/jetbrains-toolbox"
 
-echo -e "\e[94mSymlinking to $SYMLINK_DIR/jetbrains-toolbox...\e[39m"
-mkdir -p $SYMLINK_DIR
-rm "$SYMLINK_DIR/jetbrains-toolbox" 2>/dev/null || true
+echo -e "${BLUE}Symlinking to $SYMLINK_DIR/jetbrains-toolbox...${NC}"
+mkdir -p "$SYMLINK_DIR"
+rm -f "$SYMLINK_DIR/jetbrains-toolbox" 2>/dev/null || true
 ln -s "$INSTALL_DIR/jetbrains-toolbox" "$SYMLINK_DIR/jetbrains-toolbox"
 
 if [ -z "$CI" ]; then
-    echo -e "\e[94mRunning for the first time to set-up...\e[39m"
+    echo -e "${BLUE}Running for the first time to set up...${NC}"
     ( "$INSTALL_DIR/jetbrains-toolbox" & )
-    echo -e "\n\e[32mDone! JetBrains Toolbox should now be running, in your application list, and you can run it in terminal as jetbrains-toolbox (ensure that $SYMLINK_DIR is on your PATH)\e[39m\n"
+    echo -e "\n${GREEN}Done! JetBrains Toolbox should now be running, in your application list, and you can run it in terminal as jetbrains-toolbox (ensure that $SYMLINK_DIR is on your PATH)${NC}\n"
 else
-    echo -e "\n\e[32mDone! Running in a CI -- skipped launching the AppImage.\e[39m\n"
+    echo -e "\n${GREEN}Done! Running in a CI -- skipped launching the AppImage.${NC}\n"
 fi
